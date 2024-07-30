188ba2b5df80edb8827d69a0a563fd28d56f0cc8
FE4902 <isdn000123@gmail.com>
2024-07-30 23:51:46 +0900

    Feat: Todo 컴포넌트
    Feat: Todo 컴포넌트 (2)

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,30 @@
-<script></script>
+<script>
+import Todo from "./components/Todo.vue";
 
-<template></template>
+export default {
+    components: {
+        Todo,
+    },
+    data() {
+        return {
+            todos: [
+                { id: 1, text: "방 청소", isDone: true },
+                { id: 2, text: "신발 정리", isDone: false },
+                { id: 3, text: "코딩 공부", isDone: false },
+            ],
+        };
+    },
+    methods: {},
+};
+</script>
 
-<style scoped></style>
+<template>
+    <div class="wrap">
+        <Todo :todos="todos" />
+    </div>
+</template>
+
+<style lang="scss" scoped>
+.wrap {
+}
+</style>
---
+++ src/components/Todo.vue
@@ -0,0 +1,42 @@
+<script>
+export default {
+    props: {
+        todos: Array,
+    },
+};
+</script>
+
+<template>
+    <ul class="todo">
+        <li class="todo__item" v-for="todo in todos" :key="todo.id">
+            <span class="todo__text" :class="{ complete: todo.isDone }">{{
+                todo.text
+            }}</span>
+            <input
+                class="todo__checkbox"
+                type="checkbox"
+                v-model="todo.isDone"
+            />
+        </li>
+    </ul>
+</template>
+
+<style lang="scss" scoped>
+.todo {
+    &__item {
+        display: flex;
+        align-content: center;
+        gap: 8px;
+    }
+
+    &__text {
+        &.complete {
+            text-decoration: line-through;
+        }
+    }
+
+    &__checkbox {
+        accent-color: black;
+    }
+}
+</style>
